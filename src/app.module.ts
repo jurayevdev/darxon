f6afc942c0a8717a3ad311ad2dc4583bf9312a44
Davlat Jo'rayev <jurayevcoder@gmail.com>
2024-06-12 11:46:24 +0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: String(process.env.POSTGRES_PASS),
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true,
+      logging: false,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: resolve(__dirname, '..', 'uploads'),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
